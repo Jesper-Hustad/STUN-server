c6df14f2a9465268e7201b832c2c89c7ebf99ffe
Jesper Hustad <jesterbe3@gmail.com>
2021-03-24 16:09:53 +0100

    Fixed ip parsing

--- src/attributes/xorMappedAddress.ts
+++ src/attributes/xorMappedAddress.ts
@@ -13,9 +13,10 @@ export class XorMappedAddress extends Attribute {
     const portBytes = [(port & 0xff00) >> 8, (port & 0x00ff)]
     const xorPortBytes = portBytes.map((byte, i) => byte ^ MAGIC_COOKIE_BYTES[i])
 
-    const ip = Number(data.address)
-    const ipBytes = [(ip & 0xff000000) >> 24, (ip & 0x00ff0000) >> 16, (ip & 0x0000ff00) >> 8, (ip & 0x000000ff)]
-    const xorIpBytes = ipBytes.map((byte, i) => byte ^ MAGIC_COOKIE_BYTES[i])
+    const xorIpBytes = data.address
+      .split('.')
+      .map(i => Number(i))
+      .map((byte, i) => byte ^ MAGIC_COOKIE_BYTES[i])
 
     const ipv4 = 0x01
 
